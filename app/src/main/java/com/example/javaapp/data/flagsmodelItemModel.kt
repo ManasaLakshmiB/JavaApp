294af374ab17ed80dc35bdcafb5ec058b8cd0315
ManasaLakshmiB <manasalakshmi343@gmail.com>
2023-07-16 18:31:52 -0400

    removed files

--- app/src/main/java/com/example/javaapp/data/flagsmodelItemModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.javaapp.data
-
-
-import com.google.gson.annotations.SerializedName
-
-data class flagsmodelItemModel(
-    @SerializedName("flags")
-    val flags: FlagsModelX? = FlagsModelX()
-)
